fc17802e5b138c27640d227357e552cf01225f4f
0x088730 <royalsceptre88@gmail.com>
2023-05-22 11:06:36 +0900

    footer_init

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,29 +1,70 @@
 <template>
-  <div class="bg-black container-fluid flex justify-center">
+  <div class="bg-black container-fluid flex justify-center mt-[300px]">
     <div class="w-2/4 justify-center flex">
       <div class="flex w-full justify-center">
         <div class="w-1/3 p-4">
-          <div>
+          <div class="text-white text-[20px]">
             New Contract <br /> Financing
           </div>
-          <div>
+          <div class="text-white text-[20px]">
             Cash-Out <br /> Refinance
           </div>
-          <div>
+          <div class="text-white text-[20px]">
             Team
           </div>
-          <div>
+          <div class="text-white text-[20px]">
             Contract
           </div>
-          <div>
+          <div class="text-white text-[20px]">
             Blog
           </div>
-          <div>
+          <div class="text-white text-[20px]">
             Become an <br /> Affiliate
           </div>
         </div>
-        <div class="w-1/3 p-4">ghost</div>
-        <div class="w-1/3 p-4">ghost</div>
+        <div class="w-1/3 p-4">
+          <div class="text-white text-[20px]">Related Sites</div>
+          <div class="text-white text-[13px]">DVC Resale Market</div>
+          <div class="text-white text-[13px]">DVC Rental Store</div>
+          <div class="text-white text-[13px]">World of DVC <br /> Podcast</div>
+          <div class="text-white text-[13px]"></div>
+        </div>
+        <div class="w-1/3 p-4">
+          <div>Monera Financial</div>
+          <div>MAILING ADDRESS:</div>
+          <div>5937 S. Emerson Avenue, Suite 100 <br/> Indianapolis, IN 46237</div>
+          <div>
+              1-855-DVC-FUND (382-3863)
+          </div>
+          <div>info@monerafinancial.com</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="flex justify-center bg-black">
+    <div class="w-2/4 h-[1px] bg-gray-300">
+  
+    </div>
+  </div>
+  <div class="bg-black container-fluid flex justify-center">
+    <div class="w-2/4 justify-center flex">
+      <div class="flex w-full justify-center">
+        <div class="w-1/3 p-4 text-gray-500">
+          Copyright 2023 Monera Financial,<br /> LLC. All Rights Reserved
+        </div>
+        <div class="w-2/3 p-4">
+          <div>
+            <div class="text-gray-500">
+              Monera Financial, LLC is not affiliated with the Walt <br /> Disney Company or any of its subsidiaries.
+            </div>
+            <div class="text-gray-500">
+              <span>Site Map</span> • <span>Privacy Policy</span>
+            </div>
+          </div>
+          <div>
+
+          </div>
+        </div>
       </div>
     </div>
   </div>
